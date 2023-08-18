14af60cd0df65e326cd80b057abd225449aad23d
taylan32 <taylanalparslan@hotmail.com>
2023-08-18 22:53:10 +0300

    Initial commit

---
+++ src/test/java/com/example/cunder/CunderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cunder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CunderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
